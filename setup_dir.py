81db804ebf4faa99be86bf4e8127538475d8f5e8
beckylei <109053489+beckylei@users.noreply.github.com>
2024-06-12 00:16:46 +0800

    0612 upload

---
+++ setup_dir.py
@@ -0,0 +1,31 @@
+import os
+
+# make dataset dir
+root_data_dir = 'dataset'
+data_names = ['chgh', 'mmwhs', 'mmwhs2', 'segthor']
+for d in data_names:
+    data_dir = os.path.join(root_data_dir, d)
+    os.makedirs(data_dir, exist_ok=True)
+    print('mkdir data:', data_dir)
+
+
+# make model dir
+exp_dir = os.path.join('exps', 'exps')
+model_names = ['unetcnx_a1', 'swinunetr', 'unetr', 'cotr', 'attention_unet', 'unet3d', 'DynUNet', 'unest', 'testnet']
+for m in model_names:
+    for d in data_names:
+        # make model dir
+        model_exp_dir = os.path.join(exp_dir, m, d, 'tune_results')
+        os.makedirs(model_exp_dir, exist_ok=True)
+        print('mkdir exp:', model_exp_dir)
+        
+        # make infer dir
+        if d == 'mmwhs':
+            task_id = 'exp_2_tn2'
+        else:
+            task_id = 't_5' if m == 'unetcnx_a1' else 't_4'
+        model_exp_dir = os.path.join(exp_dir, m, d, 'infers', task_id)
+        os.makedirs(model_exp_dir, exist_ok=True)
+        print('mkdir exp:', model_exp_dir)
+        
+        
