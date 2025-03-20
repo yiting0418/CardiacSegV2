4fcc09ed138a295e4846f5d4be5ead90819d690a
kevin010411 <kevin010411@gmail.com>
2025-03-20 18:48:25 +0800

    Fix : tune.Tuner.restore need trainable ,so I set False

--- expers/infer_utils.py
+++ expers/infer_utils.py
@@ -14,18 +14,18 @@ def get_tune_model_dir(root_exp_dir, exp_name):
 
     print(f"Loading results from {experiment_path}...")
 
-    restored_tuner = tune.Tuner.restore(experiment_path)
+    restored_tuner = tune.Tuner.restore(experiment_path, False)
     result_grid = restored_tuner.get_results()
 
     best_result = result_grid.get_best_result(metric="tt_dice", mode="max")
-    print( f"\nBest trial {best_result.metrics['trial_id']}: ")
+    print(f"\nBest trial {best_result.metrics['trial_id']}: ")
     print('config:', best_result.metrics['config'])
     print('tt_dice:', best_result.metrics['tt_dice'])
     print('tt_hd95:', best_result.metrics['tt_hd95'])
-    if 'esc' in best_result.metrics: 
+    if 'esc' in best_result.metrics:
         print('esc:', best_result.metrics['esc'])
     print(f'best log dir:', best_result.log_dir)
-    model_dir = os.path.join( best_result.log_dir, 'models')
+    model_dir = os.path.join(best_result.log_dir, 'models')
     return model_dir
 
 
@@ -56,7 +56,7 @@ def get_pred_path(root_dir, exp_name, img_pth):
 
 def get_eval_csv_path(root_dir):
     return os.path.join(
-        root_dir, 
+        root_dir,
         'evals',
         'best_model.csv'
     )
@@ -64,24 +64,26 @@ def get_eval_csv_path(root_dir):
 
 def get_eda_eval_csv_path(root_dir):
     return os.path.join(
-        root_dir, 
+        root_dir,
         'eda_evals',
         'best_model.csv'
     )
+
 
 def get_eval_val(csv_pth, pid):
     df = pd.read_csv(csv_pth)
     idx = df['patientId'] == pid
     if 'inf_diceLV' in df[idx]:
         return {
-            'dice': df[idx].filter(regex=("inf_dice*")).T.mean().tolist()[0], 
+            'dice': df[idx].filter(regex=("inf_dice*")).T.mean().tolist()[0],
             'hd95': df[idx].filter(regex=("inf_hd*")).T.mean().tolist()[0]
         }
     else:
         return {
-            'dice': df[idx]['inf_diceC'].tolist()[0], 
+            'dice': df[idx]['inf_diceC'].tolist()[0],
             'hd95': df[idx]['inf_hd95C'].tolist()[0]
         }
+
 
 def get_slice(img, slice_idx, mode, is_trans):
     '''
@@ -93,7 +95,7 @@ def get_slice(img, slice_idx, mode, is_trans):
         img = img[:, slice_idx, :]
     else:
         img = img[slice_idx, :, :]
-    
+
     if is_trans:
         return img.T
     else:
@@ -110,7 +112,8 @@ def get_img_lbl_preds(data, exp_names, slice_idxs, mode='a', is_trans=False):
     for slice_idx in slice_idxs:
         pred_ls = []
         for exp_name in exp_names:
-              pred_ls.append(get_slice(data[exp_name], slice_idx, mode, is_trans))
+            pred_ls.append(
+                get_slice(data[exp_name], slice_idx, mode, is_trans))
         preds.append(pred_ls)
         imgs.append(get_slice(data['image'], slice_idx, mode, is_trans))
         lbls.append(get_slice(data['label'], slice_idx, mode, is_trans))
